62a3c7a4b657d620abe196ac1ee72b7898480f05
harinivassahana-max <harinivassahana@gmail.com>
2025-11-07 16:30:56 +1100

    assignment 8

---
+++ assignment 8.py
@@ -0,0 +1,15 @@
+a = 10
+b = 30
+c = 20
+
+print (a)
+print (b)
+print (c)
+
+a = b
+b = c
+c = a
+
+print("after swapping, the values of a is",a)
+print("after swapping, the value of b is",b)
+print("after swapping, the value of c is",c)
